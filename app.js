f7073542f3dff566181bb3fe11cb6a694c2b1445
philippe <philippe@SIMOCCAUCH26.lan>
2018-06-19 14:45:48 +0200

    Part11fait

---
+++ app.js
@@ -0,0 +1,37 @@
+var button ;
+var display;
+var score = 0;
+var compteur = 1;
+var prix = 50;
+var clickauto = 1;
+
+$("#bouton").click(function(){
+	score= score+1 * compteur;
+	$("#display").html(score);
+});
+
+$("#bouton2").click(function incrementer() {
+	
+		if(score>=prix){
+			compteur++;
+			score = score - prix;
+			prix = prix *2;
+			$("#display").html(score);
+		}else{
+			alert("Vous avez pas assez de COINS!!!")
+		}
+		$("#bouton2").html("Multiplicateur X " + compteur + " prix " + prix);
+});
+
+$("#autoclick").click(function(){
+	if(score>=200){
+		score = score -200;
+		setInterval(function(){
+			score= score + clickauto;
+			$("#display").html(score);
+		}, 1000)
+	}else{
+		alert("Vous avez pas assez de COINS!!!")
+	}
+	$("#display").html(score);
+});
