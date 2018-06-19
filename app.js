9ccce70287929e177de5c6b64bf8398a4cb9f782
philippe <philippe@SIMOCCAUCH26.lan>
2018-06-19 17:04:00 +0200

    afinir

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var score = 0;
 var compteur = 1;
 var prix = 50;
 var clickauto = 1;
+var Bobonus = 200;
+
 
 $("#bouton").click(function(){
 	score= score+1 * compteur;
@@ -24,8 +26,8 @@ $("#bouton2").click(function incrementer() {
 });
 
 $("#autoclick").click(function(){
-	if(score>=200){
-		score = score -200;
+	if(score>=500){
+		score = score -500;
 		$("#autoclick").attr("disabled", "disabled");
 		setInterval(function(){
 			score= score + clickauto;
@@ -38,3 +40,18 @@ $("#autoclick").click(function(){
 
 	
 });
+
+$("#bonus").click(function(){
+	if(score>=5000){
+		score = score - 5000;
+			$("#bouton").click(function(){
+				score = score + Bobonus;
+				$("#display").html(score);
+			});
+
+			seTimeout(function(){
+				Bobonus = 0;
+			} ,3000);
+	}else{
+		alert("Encore!!")
+	};
